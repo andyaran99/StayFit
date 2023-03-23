f90abc1826e9710d56f52eab80f3e890f038bd0e
andyaran99 <andreiroman9914@gmail.com>
2023-03-23 11:02:05 +0200

    commit after UserRepository.cs was implemented

--- StayFit_Data/Repositories/UserRepository.cs
+++ StayFit_Data/Repositories/UserRepository.cs
@@ -1,6 +1,47 @@
-﻿namespace StayFit.StayFit_Data.Repositories.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using StayFit.StayFit_Data.Entity;
 
-public class UserRepository
+namespace StayFit.StayFit_Data.Repositories.Repositories;
+
+
+public class UserRepository:IRepoitory<User>
 {
-    
+    private readonly Context _context;
+
+    public UserRepository(Context context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<User>> GetAll()
+    {
+        return await _context.Users
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<User> Get(int id)
+    {
+        return await _context.Users
+            .SingleAsync(user => user.Id == id);
+    } 
+
+    public async Task Delete(int id)
+    {
+        _context.Users.Remove(await Get(id));
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Add(User item)
+    {
+        _context.Users.Add(item);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<User> Update(User updatedUser)
+    {
+        _context.Users.Update(updatedUser);
+        await _context.SaveChangesAsync();
+        return updatedUser;
+    }
 }
