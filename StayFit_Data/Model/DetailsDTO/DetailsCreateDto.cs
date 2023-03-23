153102751858892b3a5a2e2afe3a266dac60fe56
andyaran99 <andreiroman9914@gmail.com>
2023-03-23 12:17:04 +0200

    commit details models

---
+++ StayFit_Data/Model/DetailsDTO/DetailsCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using StayFit.StayFit_Data.Entity;
+namespace StayFit.StayFit_Data.Model.DetailsDto;
+
+public class DetailsCreateDto
+{
+    [Required(ErrorMessage = "Body type  is required")]
+    public BodyType BodyType { get; set; }
+    [Required(ErrorMessage = "Routine Type  is required")]
+    public RoutineType RoutineType { get; set; }
+    [Required(ErrorMessage = "Date Time  is required")]
+    public DateTime DateTime{ get; set; }
+    
+    
+}
