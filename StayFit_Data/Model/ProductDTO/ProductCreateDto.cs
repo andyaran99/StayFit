feb3f55f570f17e14dc4f32c40adb4befe9ceffd
andyaran99 <andreiroman9914@gmail.com>
2023-03-23 13:24:38 +0200

    implementing models

---
+++ StayFit_Data/Model/ProductDTO/ProductCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using StayFit.StayFit_Data.Entity;
+namespace StayFit.StayFit_Data.Model.ProductDto;
+
+
+public class ProductCreateDto
+{
+    [Required(ErrorMessage = "Body type  is required")]
+    public string Title{ get; set; }
+    [Required(ErrorMessage = "Body type  is required")]
+    public string Description{ get; set; }
+    [Required(ErrorMessage = "Body type  is required")]
+    public double Price{ get; set; }
+}
---
+++ StayFit_Data/Model/ProductDTO/ProductViewDto.cs
@@ -0,0 +1,10 @@
+﻿using StayFit.StayFit_Data.Entity;
+namespace StayFit.StayFit_Data.Model.ProductDto;
+
+public class ProductViewDto
+{
+    public int Id{ get; set; }
+    public string Title{ get; set; }
+    public string Description{ get; set; }
+    public double Price{ get; set; }
+}
