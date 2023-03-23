6e2d4ab5018d2e4507afbd4a62827148489b33fd
andyaran99 <andreiroman9914@gmail.com>
2023-03-23 13:25:09 +0200

    implementing service

--- StayFit_Data/Services/ProductService.cs
+++ StayFit_Data/Services/ProductService.cs
@@ -1,6 +1,39 @@
-﻿namespace StayFit.StayFit_Data.Services;
+﻿using StayFit.StayFit_Data.Repositories;
+using StayFit.StayFit_Data.Entity;
+using StayFit.StayFit_Data.Model.ProductDto;
+using StayFit.StayFit_Data.Extension;
+
+
+namespace StayFit.StayFit_Data.Services;
 
 public class ProductService
 {
-    
+    private readonly IRepository<Product> _productRepository;
+
+    public ProductService(IRepository<Product> productRepository)
+    {
+        _productRepository = productRepository;
+    }
+    public async Task<List<ProductViewDto>> ListProducts()
+    {
+        var products = await _productRepository.GetAll();
+        return products.ToListProductViewDto();
+    }
+    public async Task<Product> NewDetails(ProductCreateDto newDetails)
+    {
+        var detailEntity = newDetails.ToProductEntity();
+        await _productRepository.Add(detailEntity);
+        return detailEntity;
+    }
+
+    public async Task DeleteDetail(int detailId)
+    {
+        await _productRepository.Delete(detailId);
+    }
+
+    public async Task<ProductViewDto> GetById(int productId)
+    {
+        var product = await _productRepository.Get(productId);
+        return product.ToProductViewDto();
+    }
 }
