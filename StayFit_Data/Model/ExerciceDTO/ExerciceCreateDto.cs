99c18333065ffc15512631a8963ad27ef6fa81f3
andyaran99 <andreiroman9914@gmail.com>
2023-04-06 10:56:32 +0300

    commit after implementing ExerciceCreateDto.cs

--- StayFit_Data/Model/ExerciceDTO/ExerciceCreateDto.cs
+++ StayFit_Data/Model/ExerciceDTO/ExerciceCreateDto.cs
@@ -1,6 +1,15 @@
-﻿namespace StayFit.StayFit_Data.Model.ExerciceDTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StayFit.StayFit_Data.Model.ExerciceDTO;
 
 public class ExerciceCreateDto
 {
+    [Required(ErrorMessage = "Body type  is required")]
+    public string Name{ get; set; }
     
+    [Required(ErrorMessage = "Body type  is required")]
+    public string Description{ get; set; }
+    
+    [Required(ErrorMessage = "Body type  is required")]
+    public DateTime DateTime{ get; set; }
 }
