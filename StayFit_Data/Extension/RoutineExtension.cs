a3a0de33ba55797649b2254804257975a9cbdaee
andyaran99 <andreiroman9914@gmail.com>
2023-04-06 12:44:29 +0300

    commit after implementing RoutineExtension.cs

--- StayFit_Data/Extension/RoutineExtension.cs
+++ StayFit_Data/Extension/RoutineExtension.cs
@@ -1,6 +1,52 @@
-﻿namespace StayFit.StayFit_Data.Extension;
+﻿using StayFit.StayFit_Data.Entity;
+using StayFit.StayFit_Data.Model.RoutineDTO;
 
-public class RoutineExtension
+namespace StayFit.StayFit_Data.Extension;
+
+public static class RoutineExtension
 {
+    public static Routine ToRoutineEntity(this RoutineViewDto routineView)
+    {
+        return new Routine
+        {
+            Id = routineView.Id,
+            RoutineType = routineView.RoutineType,
+            BodyType = routineView.BodyType,
+            DateTime = routineView.DateTime
+
+        };
+    }
+
+    public static Routine ToRoutineEntity(this RoutineCreateDto routineView)
+    {
+        return new Routine
+        {
+            RoutineType = routineView.RoutineType,
+            BodyType = routineView.BodyType,
+            DateTime = routineView.DateTime
+            
+
+        };
+    }
+
+    public static RoutineViewDto ToRoutineViewDto(this Routine routineEntity)
+    {
+        return new RoutineViewDto
+        {
+            Id = routineEntity.Id,
+            RoutineType = routineEntity.RoutineType,
+            BodyType = routineEntity.BodyType,
+            DateTime = routineEntity.DateTime
+        };
+    }
     
+    public static List<RoutineViewDto> ToListRoutineViewDto(this List<Routine> routineEntities)
+    {
+        List<RoutineViewDto> result = new List<RoutineViewDto>();
+        foreach (var routineEntity in routineEntities)
+        {
+            result.Add(ToRoutineViewDto(routineEntity));
+        }
+        return result;
+    }
 }
