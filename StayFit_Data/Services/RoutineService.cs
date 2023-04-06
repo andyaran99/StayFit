8370abd6115d19542d1cefb5d17ad45ee1f11c34
andyaran99 <andreiroman9914@gmail.com>
2023-04-06 12:47:31 +0300

    commit after implementing RoutineService.cs

--- StayFit_Data/Services/RoutineService.cs
+++ StayFit_Data/Services/RoutineService.cs
@@ -1,6 +1,38 @@
-﻿namespace StayFit.StayFit_Data.Services;
+﻿using StayFit.StayFit_Data.Repositories;
+using StayFit.StayFit_Data.Entity;
+using StayFit.StayFit_Data.Model.RoutineDTO;
+using StayFit.StayFit_Data.Extension;
+
+namespace StayFit.StayFit_Data.Services;
 
 public class RoutineService
 {
-    
+    private readonly IRepository<Routine> _routineRepository;
+
+    public RoutineService(IRepository<Routine> routineRepository)
+    {
+        _routineRepository = routineRepository;
+    }
+    public async Task<List<RoutineViewDto>> ListRoutine()
+    {
+        var routine = await _routineRepository.GetAll();
+        return routine.ToListRoutineViewDto();
+    }
+    public async Task<Routine> NewRoutine(RoutineCreateDto newDetails)
+    {
+        var detailEntity = newDetails.ToRoutineEntity();
+        await _routineRepository.Add(detailEntity);
+        return detailEntity;
+    }
+
+    public async Task DeleteRoutine(int detailId)
+    {
+        await _routineRepository.Delete(detailId);
+    }
+
+    public async Task<RoutineViewDto> GetById(int routineId)
+    {
+        var routine = await _routineRepository.Get(routineId);
+        return routine.ToRoutineViewDto();
+    }
 }
