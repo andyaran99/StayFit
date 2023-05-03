f3b9e602891b0b9bc2dfc1ef6e4738591e132d39
andyaran99 <andreiroman9914@gmail.com>
2023-05-03 09:43:58 +0300

    deleting useless classes

--- StayFit_Data/Controllers/PasswordHasher/IPasswordHasher.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StayFit.StayFit_Data.Services.PasswordHasher
-{
-    public interface IPasswordHasher
-    {
-        string HashPassword(string password);
-
-        bool VerifyPassword(string password, string passwordHash);
-    }
-}
